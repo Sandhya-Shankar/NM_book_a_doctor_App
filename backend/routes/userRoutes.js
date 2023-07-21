0d4b343a168816728b8742c2456cd2e16e33347f
awdhesh-student <69105355+awdhesh-student@users.noreply.github.com>
2023-07-21 21:08:45 +0530

    Update userRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -19,16 +19,6 @@ const authMiddleware = require("../middlewares/authMiddleware");
 
 const router = express.Router();
 
-// const storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     cb(null, './uploads/')
-//   },
-//   filename: function (req, file, cb) {
-//     const uniqueSuffix = Date.now()
-//     cb(null, uniqueSuffix +"-"+ file.originalname)
-//   }
-// })
-
 const upload = multer({ dest: 'uploads/' })
 
 router.post("/register", registerController);
